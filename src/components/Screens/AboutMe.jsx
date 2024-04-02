d0018da9c26eaf22a8b83973631dd7f1f9ab528e
Angel Ponce <78607413+Poncius1@users.noreply.github.com>
2024-04-02 13:16:45 -0600

    Add Pud Screen Styles

--- src/components/Screens/AboutMe.jsx
+++ src/components/Screens/AboutMe.jsx
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 import { Html } from '@react-three/drei';
-import { useThree} from "@react-three/fiber";
+import { useThree } from "@react-three/fiber";
 import gsap from "gsap/all";
-
 
 const AboutMe = ({ isVisible, setVisibility }) => {
   
-
   const BackPud = () => {
     setVisibility(!isVisible);
   };
 
-  const {camera} = useThree();
-  const controls = useThree((state) => state.controls)
+  const { camera } = useThree();
+  const controls = useThree((state) => state.controls);
   const timeline = gsap.timeline({
     defaults:{
       ease:"circ.out",
@@ -20,24 +18,22 @@ const AboutMe = ({ isVisible, setVisibility }) => {
     },
   });
 
-
- 
   const BackButton = () =>{
-    BackPud()
-    controls.enableRotate = true
+    BackPud();
+    controls.enableRotate = true;
 
     timeline.to(camera.position,{
       x: 122, 
       y: 76,
       z: 124,
-      
     });
-      timeline.to(controls.target,{
-        x:0,
-        y:50,
-        z:0
-      },">-3")
-    }
+    
+    timeline.to(controls.target,{
+      x: 0,
+      y: 50,
+      z: 0
+    },">-3");
+  };
 
   return (
     <>
@@ -48,28 +44,14 @@ const AboutMe = ({ isVisible, setVisibility }) => {
           rotation={[0, 1, 0]}
           transform
           occlude
-        >
-          <h2>Experience:</h2>
-          <ul>
-            <li>2019 - 2024: Ingenieria en Tecnologias Interactivas y Animacion Digital, en UVM Campús Toluca.</li>
-            <li>Agosto 2023 - Diciembre 2023: Practicas Profesionales en Macula Interactive en el juego: "Mexico,1921. A Deep Slumber."</li>
-          </ul>
-          <div className="container-with-border">
-
-            <h2>About Me:</h2>
-            <p>Hey there! I'm Angel Ponce, a lifelong gamer with a dream: to be part of the vibrant world of video game development. From console classics to cutting-edge web experiences, I've always been drawn to interactive adventures that spark the imagination.</p>
-            <p>As a software developer, I've translated my love for gaming into creating immersive experiences that captivate and delight. Whether it's crafting a compelling storyline or fine-tuning the user experience, I'm all about bringing ideas to life in ways that leave a lasting impression.</p>
-
-          </div>
-          
-          <h2>Skills:</h2>
-          <ul>
-            <li>C++</li>
-            <li>C#</li>
-            <li>JavaScript</li>
-            <li>HTML + CSS</li>
-          </ul>
-          <button className="button-scifi"onClick={BackButton}>BACK</button>
+        > 
+          <button className="btn-class-name" onClick={BackButton}>
+            <span>Back</span>
+            <svg viewBox="0 0 320 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+              <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z">
+              </path>
+            </svg>
+          </button>
         </Html>
       )}
     </>
